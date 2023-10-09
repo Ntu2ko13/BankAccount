93252281ce68baa94d671a0e89f79b0ffca25ed7
Ntuthuko Buthelezi <buthelezintuthuko1303@gmail.com>
2023-10-09 08:02:24 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        public string name;
+        public string accountNumber;
+        public double balance;
+
+        // Constructor
+        public BankAccount(string name, string accountNumber, double balance)
+        {
+            name = name;
+            accountNumber = accountNumber;
+            balance = balance;
+        }
+
+        // Deposit function
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                WriteLine($"Deposited {amount:C}. New balance is {balance:C}");
+            }
+            else
+            {
+                WriteLine("Amount to deposit must be greater than 0");
+            }
+        }
+
+        // Withdraw function
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                WriteLine($"Withdrawn {amount:C}. New balance is {balance:C}");
+            }
+            else
+            {
+                WriteLine("Insufficient balance");
+            }
+        }
+
+        // Display function
+        public void Display()
+        {
+            WriteLine($"Account Holder: {name}");
+            WriteLine($"Account Number: {accountNumber}");
+            WriteLine($"Balance: {balance:C}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+    static void Main(string[] args)
+        {
+            string name, accountNumber;
+            double balance, amount;
+            char choice;
+            BankAccount account;
+
+            WriteLine("Welcome to the Ntuthuko Bank");
+
+            do
+            {
+                WriteLine("\nMenu:");
+                WriteLine("1. Create Account");
+                WriteLine("2. Deposit");
+                WriteLine("3. Withdraw");
+                WriteLine("4. Display");
+                WriteLine("0. Exit");
+
+                Write("Enter your choice: ");
+                choice = ReadLine()[0];
+
+                switch (choice)
+                {
+                    case '1':
+                        Write("Enter name: ");
+                        name = ReadLine();
+                        Write("Enter account number: ");
+                        accountNumber = ReadLine();
+                        Write("Enter initial balance: ");
+                        balance = double.Parse(ReadLine());
+                        account = new BankAccount(name, accountNumber, balance);
+                        WriteLine("Account created successfully");
+                        break;
+                    case '2':
+                        Write("Enter amount to deposit: ");
+                        amount = double.Parse(ReadLine());
+                        account.Deposit(amount);
+                        break;
+                    case '3':
+                        Write("Enter amount to withdraw: ");
+                        amount = double.Parse(ReadLine());
+                        account.Withdraw(amount);
+                        break;
+                    case '4':
+                        account.Display();
+                        break;
+                    case '0':
+                        WriteLine("Exiting program");
+                        break;
+                    default:
+                        WriteLine("Invalid choice");
+                        break;
+                }
+            } while (choice != '0');
+        }
+    }
+}
